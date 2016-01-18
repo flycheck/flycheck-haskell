6e60ec775ec6f9637118958de9eb0c1f5d4123d0
Sebastian Wiesner <swiesner@lunaryorn.com>
2016-01-18 17:38:51 +0100

    Update copyright years

--- flycheck-haskell.el
+++ flycheck-haskell.el
@@ -1,6 +1,6 @@
 ;;; flycheck-haskell.el --- Flycheck: Automatic Haskell configuration -*- lexical-binding: t; -*-
 
-;; Copyright (C) 2014, 2015 Sebastian Wiesner <swiesner@lunaryorn.com>
+;; Copyright (C) 2014-2016 Sebastian Wiesner <swiesner@lunaryorn.com>
 ;; Copyright (C) 2015 Mark Karpov <markkarpov@opmbx.org>
 ;; Copyright (C) 2015 Michael Alan Dorman <mdorman@ironicdesign.com>
 ;; Copyright (C) 2015 Alex Rozenshteyn <rpglover64@gmail.com>
